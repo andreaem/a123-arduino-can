3861d0cd7986f4452764878da7c5cefa5e3a873a
Mike <andreaem@users.noreply.github.com>
2020-03-28 13:12:05 -0400

    BMS tested and working
    polling the BMS every 10 seconds and opening the BMS loop if the charge is above 3.57 volts in any cell.  There is still an issue with reading the detailed data

--- A123CAN/A123CAN.ino
+++ A123CAN/A123CAN.ino
@@ -2,11 +2,16 @@
 #include <mcp_can_dfs.h>
 
 // CAN-BUS Shield: Meant for A123 Modules
+
 #include <SPI.h>
 
 #include "a123_can_dfs.h"
 
+// Install the can bus seeed shield software on arduino library
 
+// TODO the balancing commands don't work, or it's not clear if it's working
+// Doesn't show cell level BMS reads, always returning only frame 2
+// This might be related to the wrong command going out for detailed response.
 
 
 unsigned char Flag_Recv = 0;
@@ -28,11 +33,37 @@ unsigned char balance_e[8] = {0x01, 0x8F, 0xC0, 0, 0, 0, 0, 0};
 unsigned char empty_message[8];
 
 
-MCP_CAN CAN(10); // Set CS to pin 10
+MCP_CAN CAN(9); // Set CS to pin 10 for Sparkfun, pin 9 for Seeed
+
+
+//Run main power and enable charging on same realy? nneds 12v not gate
+#define MAIN_POWER 4 //Main Drive Relay, high on, low off
+#define ENABLE_CHARGING 5 //BMS cutoff for charing, high on, low off
+#define CHARGING 6 //???? maybe show if charge is coming in?
+#define OVER_VOLTAGE_ALARM 7 //A battery is above OVER_VOLTAGE_ALARM_LEVEL
+
+#define OVER_VOLTAGE_ALARM_LEVEL 3.57 //volts to trigger an over voltage alarm event
 
 void setup() {
+
+
+    //LED & Relay Controls
+    pinMode(MAIN_POWER, OUTPUT);
+    pinMode(ENABLE_CHARGING, OUTPUT);
+    pinMode(CHARGING, OUTPUT);
+    pinMode(OVER_VOLTAGE_ALARM, OUTPUT);
+
+    //High is off, low on
+    digitalWrite(MAIN_POWER,HIGH);
+    digitalWrite(ENABLE_CHARGING,LOW);
+    digitalWrite(CHARGING,HIGH);
+    digitalWrite(OVER_VOLTAGE_ALARM,HIGH);
+
+
     Serial.begin(115200);
 
+    //On CANBus: Currently the 4 BMS modules of A123
+    //ALSO 1 charger 1 charge controller, can they live on 500 kbps canbus?
     while (CAN_OK != CAN.begin(CAN_500KBPS)) {
         Serial.println("CAN BUS Shield init fail");
         Serial.println("Init CAN BUS Shield again");
@@ -90,6 +121,65 @@ void setBits(unsigned int startBit, unsigned int length, unsigned char *buf, uns
       data = data >> 1;
     }
 }
+
+/*
+* Turns off when an over voltage is detected
+* CONFIRM this is not adjusting buf pointer
+*/
+
+
+void safetyShutoff(unsigned char *buf) {
+    
+    unsigned char mod_cell_undervolt = getBits(3, 1, buf);
+    unsigned char mod_cell_overvolt = getBits(2, 1, buf);
+    unsigned char mod_response_id = getBits(4, 4, buf);
+    unsigned char mod_tmp_chn = getBits(24, 3, buf);
+    unsigned char mod_therm_x = getBits(8, 8, buf) + MOD_THERM_OFFSET;
+
+    float mod_v_min = getBits(27, 13, buf) * 0.0005 + MOD_V_OFFSET;
+    float mod_v_max = getBits(43, 13, buf) * 0.0005 + MOD_V_OFFSET;
+    float mod_v_avg = getBits(59, 13, buf) * 0.0005 + MOD_V_OFFSET;
+
+    unsigned char mod_bal_cnt = getBits(40, 3, buf);
+    unsigned char mod_v_compare_oor = getBits(58, 1, buf);
+
+    //over voltage alarm
+    if (mod_v_max > OVER_VOLTAGE_ALARM_LEVEL) {
+        Serial.println("********* ALARM **********");
+        Serial.println("OVER VOLTAGE EVENT!!!: ");
+        Serial.println(mod_v_max);
+        
+        disableTheCharger(); //TURN off charger
+        turnOnAlarm(); //SOUND the alarm
+        Serial.println("********* ALARM **********");
+    }
+}
+
+void turnOnAlarm(){
+    digitalWrite(OVER_VOLTAGE_ALARM,LOW);
+}
+
+void turnOffAlarm(){
+    digitalWrite(OVER_VOLTAGE_ALARM,HIGH);
+}
+
+void enableTheCharger(){
+    digitalWrite(ENABLE_CHARGING,LOW);
+}
+
+void disableTheCharger(){
+    digitalWrite(ENABLE_CHARGING,HIGH);
+}
+
+void enableMotorController(){
+    digitalWrite(MAIN_POWER,HIGH);
+}
+void disableMotorController(){
+    digitalWrite(MAIN_POWER,LOW);
+}
+
+
+
 
 void print200Message(unsigned char *buf) {
     
@@ -151,7 +241,26 @@ void printExtendedMessage(unsigned char offset, unsigned char *buf) {
     Serial.print("Cell "); Serial.print(offset + 4); Serial.print(" State: "); Serial.print(mod_bal_cell_04); Serial.print(" Voltage: "); Serial.println(mod_v_cell_04);
 }
 
+
+
+
+unsigned long CurrentTime = millis();
+
 void loop() {
+
+    //Each battery has 2 BMS, the car shipped with 4 Modules hence 8 BMS'
+    //Relly only want ID 206 and 207 for this cellI believe the car in total has:
+    //201,202,203,204,205,206,207
+
+    //Poll every 10 seconds to see if high volatge state
+    if (millis() >= CurrentTime) {
+      CurrentTime = millis() + 10000; //wait 10 seconds
+      //this should only be polling values, not setting balance!
+      CAN.sendMsgBuf(BCM_CMD_ID, 0, 3, no_balance_e);
+      Serial.println("Sending Do Not Balance Message, Reqeusting Extended Response");
+    }
+
+  
     if (Serial.available()) {
         char key1 = Serial.read();
         // send data:  id = 0x00, standard flame, data len = 8, stmp: data buf
@@ -186,6 +295,9 @@ void loop() {
     }
     // If we get data write to buffer
     if (Flag_Recv && !unwritten_data) {                   // check if get data
+        digitalWrite(12, LOW); 
+        digitalWrite(13, LOW); 
+
         Flag_Recv = 0;
         unwritten_data = 1;
         buffersUsed = 0;
@@ -199,28 +311,50 @@ void loop() {
         }
     } else {
         for (int i = 0 ; i < buffersUsed; i++) {
-            Serial.print("Buffers Used: ");
-            Serial.println(buffersUsed);
-            Serial.print("Message: ");
-            Serial.println(i);
-            Serial.print("ID: ");
-            Serial.print(id[i], HEX);
-            Serial.print("; Len: ");
-            Serial.print(len[i]);
-            unsigned char id_mod = id[i] >> 8;
-            if (id_mod == 0x2) {
-                Serial.println();
-                print200Message(buf + 8 * i);
-            } else if (id_mod == 0x3 || id_mod == 0x4 || id_mod == 0x5 || id_mod == 0x6) {
-                Serial.println();
-                printExtendedMessage((id_mod - 3) * 4, buf + 8 * i);
-            } else {
-                Serial.print("; Data: ");
-                for (int j = 0; j<len[i]; j++) {
-                    Serial.print(buf[j + (8 * i)], HEX);Serial.print("\t");
+
+            String id_string = String(id[i],HEX);
+            int id_int = id_string.toInt();
+            if (id_int >= 201 && id_int <= 208) {
+
+                safetyShutoff(buf + 8 * i); //check if over voltage event
+    
+    
+    
+                Serial.print("Buffers Used: ");
+                Serial.println(buffersUsed);
+                Serial.print("Message: ");
+                Serial.println(i);
+                Serial.print("ID: ");
+                Serial.print(id[i], HEX);
+                Serial.print("; Len: ");
+                Serial.println(len[i]);
+                unsigned char id_mod = id[i] >> 8;
+                //Why is the id_mod never coming up w/ detailed view?    
+                //Serial.print ("id_mod: ");
+                //Serial.println(id_mod);
+               
+    
+                if (id_mod == 0x2) {
+                    Serial.println();
+                    print200Message(buf + 8 * i);
+                } else if (id_mod == 0x3 || id_mod == 0x4 || id_mod == 0x5 || id_mod == 0x6) {
+                    Serial.println();
+                    printExtendedMessage((id_mod - 3) * 4, buf + 8 * i);
+                } else {
+                    Serial.print("; Data: ");
+                    for (int j = 0; j<len[i]; j++) {
+                        Serial.print(buf[j + (8 * i)], HEX);Serial.print("\t");
+                    }
                 }
+                Serial.println();
+            } else {
+                Serial.println("--------------------BUFFER USED----------------");
+                Serial.print ("IGNORING BUFFER: ");
+                Serial.println(id[i], HEX);
+                Serial.println("-----------------------------------------------");
+
             }
-            Serial.println();
+
         }
         buffersUsed = 0;
         unwritten_data = 0;
